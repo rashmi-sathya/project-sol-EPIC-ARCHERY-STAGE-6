d65507fa831283fbc2210d22e664652ec24ec738
pro-whitehatjr <80043809+pro-whitehatjr@users.noreply.github.com>
2021-05-25 12:44:48 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -62,7 +62,7 @@ function draw() {
   fill("#FFFF");
   textAlign("center");
   textSize(40);
-  text("HUNTER ARCHER", width / 2, 100);
+  text("EPIC ARCHERY", width / 2, 100);
 
   for (var i = 0; i < playerArrows.length; i++) {
     showArrows(i, playerArrows);
